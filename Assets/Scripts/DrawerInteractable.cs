63894be0438af0e780f9025afa7d5b8fdf2fc0fb
Michael <127899373+MJR-Michael@users.noreply.github.com>
2026-02-17 23:51:57 -0500

    Added key-lock socket system with interaction layers
    Created a restricted socket system
    Used interaction layers to allow only specific objects
    Tested correct and incorrect placement behavior

---
+++ Assets/Scripts/DrawerInteractable.cs
@@ -0,0 +1,38 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class DrawerInteractable : MonoBehaviour
+{
+[SerializeField] XRSocketInteractor keySocket;
+[SerializeField] bool isLocked;
+
+    void Start()
+    {
+        if(keySocket != null)
+        {
+            keySocket.selectEntered.AddListener(OnDrawerUnlocked);
+            keySocket.selectExited.AddListener(OnDrawerLocked);
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawerUnlocked(SelectEnterEventArgs arg0)
+    {
+        isLocked = false;
+        Debug.Log("**** DRAWER UNLOCKED! ****");
+    }
+
+    private void OnDrawerLocked(SelectExitEventArgs arg0)
+    {
+        isLocked = true;
+        Debug.Log("**** DRAWER LOCKED! ****");
+    }
+}
